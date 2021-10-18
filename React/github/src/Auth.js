35dd9c1dc302350dd91f1bf2a8f2ee933e05ad20
felipecarvalhogodoi98 <felipecarvalhogodoi98@gmail.com>
2021-10-18 17:10:22 -0300

    cors github api

--- React/github/src/Auth.js
+++ React/github/src/Auth.js
@@ -1,51 +1,48 @@
-import { useLocation} from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import { useEffect } from "react";
 import axios from "axios";
 import { Spin } from "antd";
-import  { Redirect } from 'react-router-dom';
-import {words} from "lodash";
+import { Redirect } from 'react-router-dom';
+import { words } from "lodash";
 import { useAuth } from "./providers/auth";
 
-function Auth(){
-    const location = useLocation();   
-    const { setToken, isLoggedIn, setIsLoggedIn } = useAuth(); 
+function Auth() {
+    const location = useLocation();
+    const { setToken, isLoggedIn, setIsLoggedIn } = useAuth();
 
-    function getCode(){
-        const [ , code] = location.search.split('code=');
-        if(code){
+    function getCode() {
+        const [, code] = location.search.split('code=');
+        if (code) {
             getToken(code);
         }
     }
 
-    async function getToken(code){
+    async function getToken(code) {
         try {
-            const {data} = await axios.post(
-              'https://cors-anywhere.herokuapp.com/https://github.com/login/oauth/access_token',
-              { 
-                headers : {"Content-type": "application/json"},
+
+            const { data } = await axios.post('/login/oauth/access_token', {
                 client_id: process.env.REACT_APP_CLIENT_ID,
                 client_secret: process.env.REACT_APP_CLIENT_SECRET,
                 code: code,
-              },
-              
+            }
             );
             if (!data.includes('error')) {
                 const [, token] = words(data, /\=(.*?)\&/);
                 setToken(token);
                 setIsLoggedIn(true);
             }
-          } catch (error) {
-            console.log('error', error);
-          }
+        } catch (error) {
+            console.log('error', { ...error });
+        }
     }
-    
+
     useEffect(() => {
         getCode();
     })
 
-    return(
+    return (
         <>
-            {!isLoggedIn ? 
+            {!isLoggedIn ?
                 <Spin className="autenticacao" tip="Autenticando..." /> :
                 <Redirect to="/" />
             }
